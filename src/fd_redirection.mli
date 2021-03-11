0ab75e739390f2e7cab2219e262b2c30bb6fc9e1
Chris Jensen <cjjensen01@aol.com>
2021-03-11 12:52:28 +0000

    Clarify file redirection semantics

--- src/fd_redirection.mli
+++ src/fd_redirection.mli
@@ -1,5 +1,11 @@
 open! Core
 
+(** This variant determines the where the file descriptor is redirect towards.
+
+    In the [`Dev_null] case it is redirected to /dev/null, ie is ignored.
+    
+    In both [`File_append location] and [`File_truncate location] it is redirected to [location], where [location] is the absolute path to the file. 
+*)
 type t =
   [ `Dev_null
   | `File_append of string
