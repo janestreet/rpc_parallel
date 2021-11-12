a7c5b4199152bcf83d4c97d97e43cf810b27c065
Arseniy Alekseyev <aalekseyev@janestreet.com>
2021-11-12 18:38:37 +0000

    v0.15~preview.125.13+424

--- example/alternative_init.ml
+++ example/alternative_init.ml
@@ -16,7 +16,7 @@ module Worker = struct
     end
 
     module Functions
-        (C : Rpc_parallel.Creator
+        (_ : Rpc_parallel.Creator
          with type worker_state := Worker_state.t
           and type connection_state := Connection_state.t) =
     struct
